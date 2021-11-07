50c2b9bace3bd56f68fe97656efd41b869c075f4
Maj <59331721+Majus1@users.noreply.github.com>
2021-11-07 17:43:54 +0100

    Added section fadding in functuinalatay

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 console.log("Helo every one!")
 
 // ::::::::::::::::::::::::::::::
-// Header disappear on scroll
+// Header manipulation on scroll
 // ::::::::::::::::::::::::::::::
 
+// Header disappear-up  on scroll
 document.addEventListener("DOMContentLoaded", ()=> {
     let el_autohide = document.querySelector(".mg-autohide");
 
@@ -23,3 +24,59 @@ document.addEventListener("DOMContentLoaded", ()=> {
         });
     }
 });
+// Header disappear-up  on scroll
+
+// Header changes colorswhen scroled of hero section
+const header = document.querySelector(".mg-header-content");
+const sectionOne = document.querySelector("#hero-section");
+
+const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+}
+
+const sectionOneObserver = new IntersectionObserver(function(
+    entries,
+    sectionOneObserver
+  ) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            // If viewport and header are not intersecting
+            // Then add the class and change header background of header
+            header.classList.add("mg-nav-scrolled");
+        } else {
+            header.classList.remove("mg-nav-scrolled");
+        }
+    });
+  },
+  sectionOneOptions);
+  
+sectionOneObserver.observe(sectionOne);
+// Header changes colorswhen scroled of hero section
+
+// Makes elements appear from nothing
+const faders = document.querySelectorAll(".mg-fade-in") // Dodali smo .mg-fade-in vsaki sekciji ki jo želimo počasi fadati-in
+
+const appearOptions = {
+    threshold: 0.35
+};
+
+const appearOnScroll = new IntersectionObserver
+    (function(
+      entries,
+      appearOnScroll) {
+          entries.forEach(entry => {
+              if (!entry.isIntersecting) {
+                  return;
+              } else {
+                  entry.target.classList.add("appear");
+                  appearOnScroll.unobserve(entry.target);
+              }
+          })
+      },
+      appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
+
+// Makes elements appear from nothing
